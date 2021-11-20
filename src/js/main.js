77143c9d19635458c18c39c63be35f21eb9717e7
Mochamad Boval <mbovalap@gmail.com>
2021-11-20 20:57:45 +0700

    Fix vh problem on mobile browser

--- src/js/main.js
+++ src/js/main.js
@@ -92,3 +92,11 @@ document.addEventListener('DOMContentLoaded', () => {
   loadCertificates();
   loadSocial();
 });
+
+const vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  const newVh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${newVh}px`);
+});
